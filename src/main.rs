7d08d5c15f4548f939007035791c53fc0bd34ae5
Matthew Biskas <matthewbiskas@gmail.com>
2026-09-08 22:42:40 +0200

    init scanning

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,5 @@
-use std::io;
+mod scanning;
 
 fn main() {
-    let x = rand::random_range(0..10);
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Something went wrong");
-
-    let guess: i32 = guess
-        .trim()
-        .parse()
-        .expect("Please enter a number");
-
-    if guess == x {
-        println!("Correct!");
-    } else {
-        println!("Wrong!");
-    }
+    scanning::discovery::discover_ps4s();
 }
---
+++ src/scanning/discovery.rs
@@ -0,0 +1,3 @@
+pub fn discover_ps4s() {
+    println!("Scanning...");
+}
