700a5eab1dc1b527bd89d92471ce3ff6212c4778
Matthew Biskas <matthewbiskas@gmail.com>
2026-09-08 22:15:19 +0200

    guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let x = rand::random_range(0..10);
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Something went wrong");
+
+    let guess: i32 = guess
+        .trim()
+        .parse()
+        .expect("Please enter a number");
+
+    if guess == x {
+        println!("Correct!");
+    } else {
+        println!("Wrong!");
+    }
 }
