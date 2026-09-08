9dfa9cc09efb22d72865e95a006046e6835274ca
Matthew Biskas <matthewbiskas@gmail.com>
2026-09-08 23:10:02 +0200

    find interfaces

--- src/scanning/discovery.rs
+++ src/scanning/discovery.rs
@@ -1,3 +1,25 @@
+use default_net::Interface;
+
+pub fn find_interfaces() -> Vec<Interface> {
+
+    // Ignore obvious virtual interfaces or interfaces w/o ipv4
+    let interfaces: Vec<_> = default_net::get_interfaces()
+        .into_iter()
+        .filter(|interface| {
+            interface.if_type != default_net::interface::InterfaceType::Loopback
+                && !interface.ipv4.is_empty()
+                && !interface.name.starts_with("docker")
+                && !interface.name.starts_with("br-")
+                && !interface.name.starts_with("veth")
+                && !interface.name.starts_with("zt")
+        })
+        .collect();
+
+    println!("{:#?}", interfaces);
+    return interfaces
+}
+
 pub fn discover_ps4s() {
     println!("Scanning...");
+    find_interfaces();
 }
