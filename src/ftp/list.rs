19d5dc2260042c5b12de730938bb202395d2c361
Matthew Biskas <matthewbiskas@gmail.com>
2026-09-09 14:13:40 +0200

    ftp should borrow, not take ownership

--- src/ftp/list.rs
+++ src/ftp/list.rs
@@ -1,22 +1,22 @@
 use suppaftp::FtpStream;
 
-pub fn list_all(ftp: &mut FtpStream) -> Vec<String> {
+pub fn all(ftp: &mut FtpStream) -> Vec<String> {
     ftp.list(Some("/"))
         .expect("Couldn't list directory")
 }
 
 // TODO: List libraries
-pub fn list_libraries(ftp: &mut FtpStream) -> Vec<String> {
+pub fn libraries(ftp: &mut FtpStream) -> Vec<String> {
     ftp.list(Some("/system/common/lib/"))
         .expect("Couldn't list directory")
 }
 
 // TODO: List fonts
-pub fn list_fonts(ftp: &mut FtpStream) {
+pub fn fonts(ftp: &mut FtpStream) {
     return;
 }
 
 // TODO: List trophy
-pub fn list_trophy(ftp: &mut FtpStream) {
+pub fn trophy(ftp: &mut FtpStream) {
     return;
 }
