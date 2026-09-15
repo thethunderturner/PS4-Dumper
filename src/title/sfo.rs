5a43afd1d5653cf656e4fd087269a6b43c267d9f
Matthew Biskas <matthewbiskas@gmail.com>
2026-09-15 14:23:44 +0200

    sfo header

--- src/title/sfo.rs
+++ src/title/sfo.rs
@@ -1,1 +1,8 @@
 // Read: https://www.psdevwiki.com/ps4/Param.sfo
+pub struct SfoHeader {
+    pub magic: [u8; 4],
+    pub version: u32,
+    pub key_table_offset: u32,
+    pub data_table_offset: u32,
+    pub index_table_entries: u32,
+}
