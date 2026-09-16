2233e4c3708c62212b872c27a5f4b028a1f63dde
Matthew Biskas <matthewbiskas@gmail.com>
2026-09-16 12:58:38 +0200

    sfo formatter (WIP)

--- src/title/sfo.rs
+++ src/title/sfo.rs
@@ -1,13 +1,32 @@
+use std::fmt;
 use std::fs::File;
 use std::io::Read;
 
-#[derive(Debug)]
+// Read: https://www.psdevwiki.com/ps4/Param.sfo
 pub struct SfoHeader {
-    pub magic: String,
-    pub version: String,
-    pub key_table_offset: String,
-    pub data_table_offset: String,
-    pub index_table_entries: u32, // Number of entries
+    pub magic: [u8; 4],
+    pub version: u32,
+    pub key_table_offset: u32,
+    pub data_table_offset: u32,
+    pub index_table_entries: u32,
+}
+
+impl fmt::Debug for SfoHeader {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("SfoHeader")
+            .field("magic", &String::from_utf8_lossy(&self.magic[1..]))
+            .field("version", &format_args!("{:#010X}", self.version))
+            .field(
+                "key_table_offset",
+                &format_args!("{:#X}", self.key_table_offset),
+            )
+            .field(
+                "data_table_offset",
+                &format_args!("{:#X}", self.data_table_offset),
+            )
+            .field("index_table_entries", &self.index_table_entries)
+            .finish()
+    }
 }
 
 pub fn read_sfo() -> std::io::Result<()> {
@@ -24,19 +43,10 @@ pub fn read_sfo() -> std::io::Result<()> {
 
 pub fn read_header(buffer: &[u8; 20]) -> SfoHeader {
     SfoHeader {
-        magic: String::from_utf8_lossy(&buffer[1..4]).into_owned(),
-        version: format!(
-            "0x{:08X}",
-            u32::from_le_bytes(buffer[4..8].try_into().unwrap())
-        ),
-        key_table_offset: format!(
-            "0x{:X}",
-            u32::from_le_bytes(buffer[8..12].try_into().unwrap())
-        ),
-        data_table_offset: format!(
-            "0x{:X}",
-            u32::from_le_bytes(buffer[12..16].try_into().unwrap())
-        ),
+        magic: buffer[0..4].try_into().unwrap(),
+        version: u32::from_le_bytes(buffer[4..8].try_into().unwrap()),
+        key_table_offset: u32::from_le_bytes(buffer[8..12].try_into().unwrap()),
+        data_table_offset: u32::from_le_bytes(buffer[12..16].try_into().unwrap()),
         index_table_entries: u32::from_le_bytes(buffer[16..20].try_into().unwrap()),
     }
 }
